a65adca39e0f8d1f49f444390c7435b9ff9cbaee
Andrew Star <andrew005star@gmail.com>
2026-03-18 15:39:27 +0300

    start2

--- FundMe.sol
+++ FundMe.sol
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 contract FundMe {
-    
+    // send funds into our contract
+    function fund() public {}
+    // owner can withdraw funds
+    /*function withdraw() public {}*/
 }
