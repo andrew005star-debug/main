9f2bf2753be15e39a116341c21412e1cac82e21f
Andrew Star <andrew005star@gmail.com>
2026-03-18 15:36:25 +0300

    start

--- Balances.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-contract SimpleStorage {
-    mapping (string => uint256 ) public base_balance;
-    function addBalance(string memory _address, uint256 _balance) public {
-        base_balance[_address] = _balance;
-    }
-}
--- Squared.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
-import {SimpleStorage} from "./SimpleStorage.sol";
-
-contract Squared is SimpleStorage {
-    function store(uint256 _newFavNumber) public override {
-    myFavoriteNumber = _newFavNumber * _newFavNumber;
-    }
-}
